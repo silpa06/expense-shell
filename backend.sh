d10164d0d904d90a0bae3e6dc71c43d462c3c22f
Silpa <silpa.jr@gmail.com>
2024-08-29 11:58:37 +0800

    filename

---
+++ backend.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/expense"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R please run the script with root previleages $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.. $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is ..$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATION $? "nodejs module disabled is"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATION $? "nodejs:20 module enabled"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATION $? "nodejs installation"
+
+useradd expense
+VALIDATION $? "adding expense app user"
+
+
+
+
+

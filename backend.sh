327de36a9643ba69d71bead9046f4bc5f3ed252b
Silpa <silpa.jr@gmail.com>
2024-08-29 12:34:35 +0800

    filename

--- backend.sh
+++ backend.sh
@@ -37,9 +37,42 @@ VALIDATION $? "nodejs:20 module enabled"
 
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATION $? "nodejs installation"
+id expense &>>$LOG_FILE
+if [ $? -ne 0 ]
+then   
+    echo -e "expense user is not exist $G CREATING $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATION $? "adding expense app user"
+else
+    echo -e "expense user is already exist $Y SKIPPING $N"
+fi
 
-useradd expense
-VALIDATION $? "adding expense app user"
+mkdir -p /app
+VALIDATION $? "creating /app"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATION $? "downloading backend application code"
+
+cd /app
+rm -rf /app/* #removing the existing code
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATION $? "extracting backend application code"
+npm install &>>$LOG_FILE
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+dnf install mysql -y &>>$LOG_FILE
+VALIDATION $> "mysql installation"
+mysql -h db.devteck.xyz -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATION $? "validate schema loading"
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATION $? "daemon reload"
+systemctl enable backend &>>$LOG_FILE
+VALIDATION $? "enabled backend"
+systemctl restart backend &>>$LOG_FILE
+VALIDATION $? "restarting backend"
+
+
+
 
 
 

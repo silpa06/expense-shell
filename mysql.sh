4fb39b7e01d8811dd6954984ffa43f71e2423b2b
Silpa <silpa.jr@gmail.com>
2024-08-29 08:56:51 +0800

    filename

--- mysql.sh
+++ mysql.sh
@@ -5,10 +5,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOG_FOLDER="/var/log/expense"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R please run the script with root previleages $N"
+        echo -e "$R please run the script with root previleages $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -16,12 +20,13 @@ CHECK_ROOT(){
 VALIDATION(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is.. $R FAILED $N"
+        echo -e "$2 is.. $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is ..$G SUCCESS $N"
+        echo -e "$2 is ..$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 CHECK_ROOT
-dnf install mysql-server -y
+dnf install mysql-server -y &>> tee -a $LOG_FILE
 VALIDATION $? "MYSQL server installation"
